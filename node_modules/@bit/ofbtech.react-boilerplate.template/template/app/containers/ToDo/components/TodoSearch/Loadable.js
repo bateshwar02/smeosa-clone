f230e9d8da78b3e0fd2d56b0c075f8753f0078ad
Bateshwar Mishra <bnmkrishna123@gmail.com>
2020-01-21 18:56:05 +0530

    remove node module

--- node_modules/@bit/ofbtech.react-boilerplate.template/template/app/containers/ToDo/components/TodoSearch/Loadable.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * Asynchronously loads the component for TodoSearch
- *
- */
-
-import loadable from '../../../../utils/loadable';
-
-export default loadable(() => import('./index'));
--- node_modules/@bit/ofbtech.react-boilerplate.template/template/server/services/todos.js
+++
@@ -1,9 +0,0 @@
-const API = require('../lib/api');
-
-// TODO
-module.exports = {
-    getToDos(callback, headers) {
-        const url = '/todos';
-        return API.get(url, callback, headers);
-    },
-};
